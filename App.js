5b6db0346aee008c427ab133b4cc31d56c24579b
JohnyCruz37 <johnygoncalvescruz@gmail.com>
2024-08-30 11:27:55 -0300

    Adição do projeto cronometro

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: '',
+      input: ''
+    }
+
+    this.entrar = this.entrar.bind(this);
+  }
+
+  entrar(){
+    if(this.state.input === ''){
+      alert('Por favor, digite seu nome!');
+      return;  
+    } 
+    this.setState({nome: `Bem-vindo, ${this.state.input}!`});
+  }
+  render() {
+    return (
+      <View style={style.container}>
+        <TextInput 
+        style={style.input}
+        placeholder="Digite seu nome"
+        onChangeText={(nome) => this.setState({input: nome})}
+        />
+
+        <Button title='Inserir' onPress={this.entrar} /> 
+
+        <Text style={style.text} > {this.state.nome}</Text>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
+
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  input:{
+    width: '80%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
+
+export default App;
